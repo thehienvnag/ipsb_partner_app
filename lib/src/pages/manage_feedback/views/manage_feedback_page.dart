73ebffc2e3575a20a5db373e41e112e7516ce0b0
krisode <huyttdse140080@fpt.edu.vn>
2021-07-21 11:37:37 +0700

    feature/HuyTTD/Fix-manage-feedback

--- lib/src/pages/manage_feedback/views/manage_feedback_page.dart
+++ lib/src/pages/manage_feedback/views/manage_feedback_page.dart
@@ -104,7 +104,7 @@ Widget buildFeedbacks(BuildContext context, List<CouponInUse> listFeedBack) {
                       SizedBox(height: 18),
                       Text(
                         Formatter.dateCaculator(feedback.feedBackDate.toString()),
-                        style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),
+                        style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                       ),
                     ],
                   ),
